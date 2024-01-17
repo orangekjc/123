657f4cf3a80c395451e3cb97756536a7df534710
Jia Cheng <jiacheng@open.gov.sg>
2024-01-17 15:39:53 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import { getBotToken } from 'nestjs-telegraf';
 import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
   app.use(cookieParser());
 
   const bot = app.get(getBotToken());
-  app.use(bot.webhookCallback(process.env.BOT_PATH));
+  app.use(bot.webhookCallback(configService.get<string>('bot.path')));
   await app.listen(3000);
 }
 bootstrap();

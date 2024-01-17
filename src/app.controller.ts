64fc87f94d01a2a88b0c606420f71a0c03f1909c
Jia Cheng <jiacheng@open.gov.sg>
2024-01-17 11:23:00 +0800

    test 6

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Req() req: Request): string {
     return (
       this.appService.getHello() +
       ' ' +
       process.env.VERCEL_URL +
       ' ' +
-      process.env.BOT_DOMAIN
+      process.env.BOT_DOMAIN +
+      ' ' +
+      req.headers.host
     );
   }
 }
